d8182d2a7b76f4aca4bb5c088df733de95286a4a
Vu Tuan Hai <haivutuan93@gmail.com>
2022-09-10 12:23:57 +0700

    test auto

--- src/main/java/com/springboot/app/WelcomeController.java
+++ src/main/java/com/springboot/app/WelcomeController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class WelcomeController {
  @GetMapping("/welcome")
 	public String welcome() {
-	 return "welcome to spring boot app k8s-demo";
+	 return "welcome to spring boot app k8s-demo111";
  }
 }
